3fc122c7258a987d344408f9546bf9c58ada6adc
Michael Pulsifer <pulsifer.michael@dol.gov>
2015-01-02 15:20:30 -0500

    Initial commit.

---
+++ Public Data Listing Consolidator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Public Data Listing Consolidator
+//
+//  Created by Michael Pulsifer on 1/2/15.
+//  Copyright (c) 2015 U.S. Department of Labor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
